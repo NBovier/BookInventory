18b5ff5de2937692b891f3e180e4752d1d911cee
Almin20 <salcinovicalmin20@gmail.com>
2019-11-06 16:33:38 +0100

    ajout layout add shelfloc

---
+++ app/src/main/java/com/example/nathan_almin_bookinventory/ui/main/shelfloc_add.java
@@ -0,0 +1,20 @@
+package com.example.nathan_almin_bookinventory.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.nathan_almin_bookinventory.R;
+
+public class shelfloc_add extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shelfloc_add);
+    }
+
+    public void addShelfLoc(View view) {
+    }
+}
